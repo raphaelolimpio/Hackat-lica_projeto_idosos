7331d1c38c21060044f257d8ad2d091f2980300a
raphaelolimpio <76962348+raphaelolimpio@users.noreply.github.com>
2024-10-10 14:48:34 -0300

    criação das ultimas paginas e seus layouts, indo para fase de beckand

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:appidoso1/views/contacts/contacts_view_mode.dart';
 import 'package:appidoso1/views/home/home.dart';
 import 'package:appidoso1/views/home/home_view_mode.dart';
+import 'package:appidoso1/views/inscription/inscription_view_mode.dart';
 import 'package:appidoso1/views/login/login_view_mode.dart';
 import 'package:appidoso1/views/shedule/shedule_view_mode.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: SheduleViewMode(),
+    home: InscriptionViewMode(),
   ));
 }
